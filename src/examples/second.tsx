20993f772751a94005534c7580b5e9a5cf95e75f
Polo <artem-gribanof@yandex.ru>
2023-05-05 21:49:11 +0300

    деплой

--- src/examples/second.tsx
+++ src/examples/second.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link, useRouteMatch, useParams } from "react-router-dom";
 
 interface RouteParams {
 	topicId: string;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,4 @@
 import ReactDOM from "react-dom/client";
-import { App } from "./examples/first";
 import { App2 } from "./examples/second";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
